fbf0a09f5b7385acc52ee02bcce21a5d227392cd
AnNable00 <101518922+AnNable00@users.noreply.github.com>
2022-05-27 16:20:46 +0300

    Update info_map_script.mjs

--- info/info_map_script.mjs
+++ info/info_map_script.mjs
@@ -6,6 +6,15 @@ const map = new mapboxgl.Map({
     zoom: 14 // starting zoom
 });
 
-const marker2 = new mapboxgl.Marker({ color: 'red' })
+let popup = new mapboxgl.Popup({offset:[0,-20], closeButton:false, closeOnClick:false})
+.setHTML('<p>Αγίου Ανδρέου 9</p>')
+
+const marker = new mapboxgl.Marker({ color: 'red' })
 .setLngLat([21.737442404704655, 38.250841797737515])
+.setPopup(popup)
 .addTo(map);
+
+const markerDiv = marker.getElement();
+
+markerDiv.addEventListener('mouseenter', () => marker.togglePopup());
+markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
