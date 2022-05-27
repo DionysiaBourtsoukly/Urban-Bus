2a329aaa7154da22e0239dfd103db3f755d8ff82
AnNable00 <101518922+AnNable00@users.noreply.github.com>
2022-05-27 18:28:01 +0300

    Update info_map_script.mjs

--- info/info_map_script.mjs
+++ info/info_map_script.mjs
@@ -6,15 +6,32 @@ const map = new mapboxgl.Map({
     zoom: 14 // starting zoom
 });
 
-let popup = new mapboxgl.Popup({offset:[0,-20], closeButton:false, closeOnClick:false})
-.setHTML('<p>Αγίου Ανδρέου 9</p>')
+const mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+mapboxClient.geocoding
+    .forwardGeocode({
+        query: 'Αγίου Ανδρέου 9, Patras, Greece',
+        autocomplete: false,
+        limit: 1
+    })
+    .send()
+    .then((response) => {
+        if (
+            !response ||
+            !response.body ||
+            !response.body.features ||
+            !response.body.features.length
+        ) {
+            console.error('Invalid response:');
+            console.error(response);
+            return;
+    }
+    const feature = response.body.features[0];
+    let text = "Αγίου Ανδρέου 9";
+    let popup = new mapboxgl.Popup({offset:[0,-20], closeButton:false, closeOnClick:false})
+    .setHTML(text)
+    let marker = new mapboxgl.Marker({color:'red'}).setLngLat(feature.center).setPopup(popup).addTo(map);
+    let markerDiv = marker.getElement();
+    markerDiv.addEventListener('mouseenter', () => marker.togglePopup());
+    markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
+})
 
-const marker = new mapboxgl.Marker({ color: 'red' })
-.setLngLat([21.737442404704655, 38.250841797737515])
-.setPopup(popup)
-.addTo(map);
-
-const markerDiv = marker.getElement();
-
-markerDiv.addEventListener('mouseenter', () => marker.togglePopup());
-markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
