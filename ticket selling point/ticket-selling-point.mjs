b73d351a37d28605ac9926e3c0af5626c90ba77a
DionysiaMp <103177508+DionysiaMp@users.noreply.github.com>
2022-05-31 00:59:53 +0300

    Delete ticket selling point directory

--- ticket selling point/ticket-selling-point.mjs
+++
@@ -1,7 +0,0 @@
-let places = document.querySelectorAll(".places #a");
-for(let i of places){
-    i.addEventListener('click',()=>{
-        //placeholder function - map tag will appear
-        console.log(i.textContent);
-    })
-}
